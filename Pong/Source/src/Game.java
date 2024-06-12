4beb45227decd5a36f0a331cb40a8ac693aa03c4
manugcr <mgilcernich@gmail.com>
2024-06-12 17:23:00 -0300

    CL: - Added pause menu - Added new functions to render pause menu - Imported font handling libraries

--- Pong/Source/src/Game.java
+++ Pong/Source/src/Game.java
@@ -1,6 +1,8 @@
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -9,250 +11,223 @@ import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
-/**
- * The `Game` class represents the main game logic and functionality.
- * It extends the `Canvas` class and implements the `Runnable` and `KeyListener` interfaces.
- * The game is responsible for rendering the game objects, handling user input, and updating the game state.
- */
 public class Game extends Canvas implements Runnable, KeyListener, ScoreObserver {
-	
-	boolean isRunning = false;
-	
-	private BufferStrategy bs;
-	private BufferedImage image;
-	
-	public static Player player;
-	public static Enemy enemy;
-	public static Ball ball;
-	
-	public static final int WIDTH = 240;
-	public static final int HEIGHT = 160;
-	private final int SCALE = 4;
-	
-	private final int SPEED = 4;
-	private final int FPS = 60;
-	private final long ns = 1000000000/FPS;
-	private long lastTime = System.nanoTime();
-	
-	public boolean rightPressedPlayer = false;
-	public boolean leftPressedPlayer = false;
 
-	private int playerScore = 0;
+    boolean isRunning = false;
+    boolean isPaused = false;
+
+    private BufferStrategy bs;
+    private BufferedImage image;
+
+    public static Player player;
+    public static Enemy enemy;
+    public static Ball ball;
+
+    public static final int WIDTH = 240;
+    public static final int HEIGHT = 160;
+    private final int SCALE = 4;
+
+    private final int SPEED = 4;
+    private final int FPS = 60;
+    private final long ns = 1000000000 / FPS;
+    private long lastTime = System.nanoTime();
+
+    public boolean rightPressedPlayer = false;
+    public boolean leftPressedPlayer = false;
+
+    private int playerScore = 0;
     private int enemyScore = 0;
-	
 
-	/**
-	 * @brief The main entry point for the Pong game.
-	 * 
-	 * Creates a new instance of the Game class, starts a new thread for the game loop,
-	 * and initializes the game frame.
-	 *
-	 * @param args The command line arguments.
-	 */
-	public static void main(String[] args) {
-		Game game = new Game();
-		Thread thread = new Thread(game);
-		JFrame frame = new JFrame();
-		
-		game.start();
-		game.startFrame(frame);
-		thread.start();
-	}
-	
+    private String[] menuOptions = {"Resume", "Reset", "Exit"};
+    private int selectedOption = 0;
 
-	/**
-	 * @brief The Game class represents the main game window.
-	 * 
-	 * It extends the JPanel class and implements the KeyListener interface.
-	 */
-	Game() {
-		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
-		this.addKeyListener(this);
-		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-	}
-	
+    public static void main(String[] args) {
+        Game game = new Game();
+        Thread thread = new Thread(game);
+        JFrame frame = new JFrame();
 
-	/**
-	 * @brief Sets up the game
-	 * 
-	 * Starts the game by initializing the player, enemy, and ball objects.
-	 * 
-	 * This method should be called before any game logic is executed.
-	 */
-	public synchronized void start() {
-		player = new Player(100, 155);
-		enemy = new Enemy(100, 0);
-		ball = new Ball();
-		ball.addObserver(this);
-		isRunning = true;
-	}
-	
+        game.start();
+        game.startFrame(frame);
+        thread.start();
+    }
 
-	/**
-	 * @brief Sets the game panel 
-	 * 
-	 * Adds the game panel to the specified JFrame, configures the frame, and makes it visible.
-	 * 
-	 * @param frame the JFrame to start the game in
-	 */
-	public void startFrame(JFrame frame) {
-		frame.add(this);
-		frame.pack();
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-	}
-	
-	
-	/**
-	 * @brief Updates the game state for each frame.
-	 * 
-	 * If the right arrow key is pressed, the player moves to the right.
-	 * If the left arrow key is pressed, the player moves to the left.
-	 */
-	public void tick() {
-		if (rightPressedPlayer) {		
-			player.rightPressed();	
-		} else if (leftPressedPlayer) {
-			player.leftPressed();
-		}
-	}
-	
+    Game() {
+        this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        this.addKeyListener(this);
+        image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    }
 
-	/**
-	 * @brief Renders the game by drawing the game elements on the screen.
-	 */
-	public void render() {
-		bs = this.getBufferStrategy();
-	
-		if (bs == null) {
-			this.createBufferStrategy(3);
-			return;
-		}
-	
-		Graphics g = image.getGraphics();
-	
-		// Background
-		g.setColor(new Color(0, 0, 0));
-		g.fillRect(0, 0, WIDTH, HEIGHT);
-	
-		player.render(g);
-		ball.render(g);
-		enemy.render(g);
-	
-		// Draw horizontal dotted line in the middle
-		g.setColor(Color.WHITE);
-		for (int x = 0; x < WIDTH; x += 10) {
-			g.fillRect(x, HEIGHT/2, 5, 1); // Draw a short white line at intervals of 10 pixels
-		}
-	
-    	// Draw player score above the line
-    	g.drawString(String.valueOf(playerScore), WIDTH/2-5, HEIGHT/2+15);
+    public synchronized void start() {
+        player = new Player(100, 155);
+        enemy = new Enemy(100, 0);
+        ball = new Ball();
+        ball.addObserver(this);
+        isRunning = true;
+    }
 
-    	// Draw enemy score below the line
-    	g.drawString(String.valueOf(enemyScore), WIDTH/2-5, HEIGHT/2-5);
-	
-		g = bs.getDrawGraphics();
-	
-		g.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
-		bs.show();
-	}
-	
-	
-	
-	
-	/**
-	 * @brief Starts the buffer strategy for rendering the game.
-	 * 
-	 * Buffer strategy is used to avoid flickering in the game rendering.
-	 * 
-	 * @param bs the buffer strategy to start
-	 */
-	public void startBufferStrategy(BufferStrategy bs) {
-		if (bs == null) {
-			this.createBufferStrategy(3);	
-			return;
-		}
-		
-		bs.show();
-	}
-	
+    public void startFrame(JFrame frame) {
+        frame.add(this);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 
-	/**
-	 * @brief main game loop
-	 * 
-	 * Runs the game loop, continuously updating and rendering the game.
-	 */
-	public void run() {
-		while (isRunning) {
-			this.requestFocus();
-			long now = System.nanoTime();
+    public void tick() {
+        if (isPaused) {
+            return;
+        }
+
+        if (rightPressedPlayer) {
+            player.rightPressed();
+        } else if (leftPressedPlayer) {
+            player.leftPressed();
+        }
+    }
+
+    public void render() {
+        bs = this.getBufferStrategy();
+
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = image.getGraphics();
+
+        g.setColor(new Color(0, 0, 0));
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        if (isPaused) {
+            renderPauseMenu(g);
+        } else {
+            player.render(g);
+            ball.render(g);
+            enemy.render(g);
 			
-			if (now - lastTime >= ns/SPEED) {
-				tick();
-				render();				
-				lastTime = now;
-			}			
-		}	
-	}
+			g.drawString(String.valueOf(playerScore), WIDTH / 2 - 5, HEIGHT / 2 + 15);
+			g.drawString(String.valueOf(enemyScore), WIDTH / 2 - 5, HEIGHT / 2 - 5);
+        	
+			g.setColor(Color.WHITE);
+        	for (int x = 0; x < WIDTH; x += 10) {
+            	g.fillRect(x, HEIGHT / 2, 5, 1);
+        	}
+        }
 
+        g = bs.getDrawGraphics();
+        g.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+        bs.show();
+    }
 
-	/**
-	 * @brief Invoked when a key is pressed.
-	 * 
-	 * @param e the KeyEvent object representing the key event
-	 */
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			rightPressedPlayer = true;
+    private void renderPauseMenu(Graphics g) {
+        int menuWidth = 180;
+        int menuHeight = 90;
+        int menuX = (WIDTH - menuWidth) / 2;
+        int menuY = (HEIGHT - menuHeight) / 2;
+
+        g.setColor(Color.BLACK);
+        g.fillRect(menuX, menuY, menuWidth, menuHeight);
+
+        g.setColor(Color.WHITE);
+        g.drawRect(menuX, menuY, menuWidth, menuHeight); // Add white border
+        g.setFont(new Font("Arial", Font.BOLD, 16));
+
+		FontMetrics fm = g.getFontMetrics();
+		int textHeight = fm.getAscent();
+
+		int verticalSpacing = 5; // Adjust spacing between options
+
+		for (int i = 0; i < menuOptions.length; i++) {
+			int textWidth = fm.stringWidth(menuOptions[i]);
+			int textX = menuX + (menuWidth - textWidth) / 2;
+			int textY = menuY + menuHeight / 2 - ((menuOptions.length - 1) * textHeight / 2) + i * (textHeight + verticalSpacing);
+	
+			if (i == selectedOption) {
+				g.setColor(Color.RED);
+			} else {
+				g.setColor(Color.WHITE);
+			}
+	
+			g.drawString(menuOptions[i], textX, textY);
 		}
-		else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-			leftPressedPlayer = true;
-		}	
-	}
+    }
 
+    public void run() {
+        while (isRunning) {
+            this.requestFocus();
+            long now = System.nanoTime();
 
-	/**
-	 * @brief Invoked when a key is released.
-	 * 
-	 * @param e the KeyEvent object containing information about the key event
-	 */
-	@Override
-	public void keyReleased(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			rightPressedPlayer = false;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-			leftPressedPlayer = false;
-		}
-	}
+            if (now - lastTime >= ns / SPEED) {
+                tick();
+                render();
+                lastTime = now;
+            }
+        }
+    }
 
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (isPaused) {
+            if (e.getKeyCode() == KeyEvent.VK_UP) {
+                selectedOption = (selectedOption - 1 + menuOptions.length) % menuOptions.length;
+            } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                selectedOption = (selectedOption + 1) % menuOptions.length;
+            } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                handleMenuSelection();
+            }
+            return;
+        }
 
-	/**
-	 * @brief Invoked when a key is typed. 
-	 * 
-	 * This method is not used in this implementation.
-	 *
-	 * @param e the KeyEvent object containing information about the key event
-	 */
-	@Override
-	public void keyTyped(KeyEvent e) {
-		
-	}
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            rightPressedPlayer = true;
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            leftPressedPlayer = true;
+        } else if (e.getKeyCode() == KeyEvent.VK_P) {
+            isPaused = !isPaused;
+        }
+    }
 
-	/**
-	 * @brief Updates the player and enemy scores.
-	 * 
-	 * Overrides the updateScore method from the ScoreObserver interface.
-	 * 
-	 * @param playerScore the player's score
-	 */
-	@Override
+    private void handleMenuSelection() {
+        switch (selectedOption) {
+            case 0:
+                isPaused = false;
+                break;
+            case 1:
+                resetGame();
+                isPaused = false;
+                break;
+            case 2:
+                System.exit(0);
+                break;
+        }
+    }
+
+    private void resetGame() {
+        player = new Player(100, 155);
+        enemy = new Enemy(100, 0);
+        ball = new Ball();
+        ball.addObserver(this);
+        playerScore = 0;
+        enemyScore = 0;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            rightPressedPlayer = false;
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            leftPressedPlayer = false;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
     public void updateScore(int playerScore, int enemyScore) {
         this.playerScore = playerScore;
         this.enemyScore = enemyScore;
     }
-	
 }
